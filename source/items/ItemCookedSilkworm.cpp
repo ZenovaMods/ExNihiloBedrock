ad18761e8544df91b8d9e1d31f1ddf1b83b8b0ac
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2020-06-08 14:07:44 -0500

    Add cooked silkworm

---
+++ source/items/ItemCookedSilkworm.cpp
@@ -0,0 +1,9 @@
+#include "ItemCookedSilkworm.h"
+
+#include "minecraft/FoodItemComponent.h"
+
+ItemCookedSilkworm::ItemCookedSilkworm(const std::string& name, int id) : Item(name, id) {
+	setUseAnimation(UseAnimation::EAT);
+	setMaxUseDuration(1);
+	mFoodComponent = std::make_unique<FoodItemComponent>(*this, 2, 0.6F);
+}
